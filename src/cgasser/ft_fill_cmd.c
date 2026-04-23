19519bef02a0ebd7d20207115c2791dfbaccbbb5
Cyrille Gasser <cgasser@c2r2s7.42lausanne.ch>
2026-04-23 14:41:13 +0200

    fixed minor bug

--- src/cgasser/ft_fill_cmd.c
+++ src/cgasser/ft_fill_cmd.c
@@ -8,7 +8,7 @@ void	ft_fill_cmd(t_cmd *cmd, t_token *token)
 
 	word = NULL;
 	word = malloc(sizeof(char) * (ft_strlen(token->word) + 1));
-	strlcpy(word, token->word, ft_strlen(token->word) + 1);
+	ft_strlcpy(word, token->word, ft_strlen(token->word) + 1);
 	if (token->type == FLAG)
 		cmd->flag = ft_arrayadd_back(cmd->flag, word);
 	else
