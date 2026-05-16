4d8ceeaf47f8fdb8fc6143d35714ef974233c0b4
atrombel <ador.trombella@gmail.com>
2026-05-16 20:52:49 +0200

    cd correction and resistent against unset HOME, ft_readline added functin that read and display the current working directory path as a ormal shell, done with strong security. echo security checked and updated. main security improvment added.

--- src/atrombel/ft_cd.c
+++ src/atrombel/ft_cd.c
@@ -1,18 +1,22 @@
 #include "minishell.h"
 #include "atrombel.h"
 
+// function that updates env if cd worked
 int	cd_success(t_env *env, char *old_pwd)
 {
-	char	new_wd[PATH_MAX];
+	char	*new_wd;
 
-	ft_change_value_env(env, "OLDPWD", old_pwd);
-	free(old_pwd);
-	if (getcwd(new_wd, PATH_MAX) == NULL)
+	new_wd = getcwd(NULL, 0);
+	if (!new_wd)
 	{
 		perror("minishell:cd:");
+		free(old_pwd);
 		return (1);
 	}
+	ft_change_value_env(env, "OLDPWD", old_pwd);
+	free(old_pwd);
 	ft_change_value_env(env, "PWD", new_wd);
+	free (new_wd);
 	return (0);
 }
 
@@ -39,28 +43,46 @@ void	ft_chdir(char	*path, t_data *data, t_env *env)
 	}
 }
 
-//cd args change the working directory
-void	ft_cd(t_cmd *content, t_data *data, t_env *env)
+// check if mulitple args or not
+static int	nbr_args_check(t_list	*args, t_data *data)
 {
-	char	*path;
-	t_list	*args;
-
-	args = content->args;
-	if (args == NULL || (args->content) == NULL || ((char *)args->content)[0] == '\0')
-	{
-		path = ft_get_value_env(env,"HOME");
-		ft_chdir(path, data, env);
-		return ;
-	}
-	else
-		path = ((char *)args->content);
-	if (((char *)args->content)[0] && ((char *)args->content)[1])
+	if (args && args->content && args->next)
 	{
 		data->last_exit_status = 1;
-
 		ft_putstr_fd("minishell: cd: too many arguments\n", 2);
+		return (1);
+	}
+	return (0);
+}
+
+//function that determine if a path if given or not
+static char	*path_define_cd(t_list	*args, t_env *env)
+{
+	char	*path;
+
+	path = NULL;
+	if (args == NULL || (args->content) == NULL || ((char *)args->content)[0] == '\0')
+		path = ft_get_value_env(env,"HOME");
+	else
+		path = ((char *)args->content);
+	return (path);
+}
+
+//cd args change the working directory
+void	ft_cd(t_cmd *tcmd, t_data *data, t_env *env)
+{
+	char	*path;
+	t_list	*arg;
+
+	arg = tcmd->args;
+	if (nbr_args_check(arg, data) == 1)
+		return ;
+	path = path_define_cd(arg, env);
+	if (!path)// strong against unset HOME
+	{
+		data->last_exit_status = 1;
+		ft_putstr_fd("minishell: cd: HOME not set\n", 2);
 		return ;
 	}
 	ft_chdir(path, data, env);
 }
-
