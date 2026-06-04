760f4e3d813a4e31d51feafe62fc2e5b55531e62
weezywooz <189519321+weezywooz@users.noreply.github.com>
2026-06-04 13:24:58 +0200

    WIP: multiple env variable in a row

--- src/cgasser/ft_expand_var.c
+++ src/cgasser/ft_expand_var.c
@@ -67,7 +67,8 @@ int	ft_var_name_len(char *str, int i)
 	int	len;
 
 	len = 0;
-	while (!ft_isquote(str[i + len]) && str[i + len] != ' ' && str[i + len] != '\0')
+	while (!ft_isquote(str[i + len]) && str[i + len] != ' ' \
+		&& str[i + len] != '\0')
 		len++;
 	return (len);
 }
